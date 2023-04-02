47247c80ad8e055c3dd0a39cd70e9c402a050f3f
Vitaly Kovalenko <rtm_wrk@rambler.ru>
2023-04-02 19:58:24 +0300

    Add JaCoCo plugin and success test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Assertions;
 
 public class StatisticsServiceTest {
 
+    // 1-й элемент массива и содержит самое большое значение,
+    // условие "currentMax < income" не получает значение "истина",
+    // посему строка № 8 сервисного класса не отрабатывается вообще
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
@@ -17,4 +20,20 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    // Протестируем класс на работу, когда элемент с максимальным значением
+    // будет не первым в массиве, для чего изменим значение 0-го элемента массива.
+    // Єто приведет к отработке ветки сервисного класса "if (currentMax < income)..."
+    @Test
+    void findMaxIfCurrentMaxLessIncome() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {2, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
